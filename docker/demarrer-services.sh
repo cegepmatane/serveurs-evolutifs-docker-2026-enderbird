fd95582bbc7866d04b1df3f62910b542deb720b8
ender_bird_ <cedricsimard28@gmail.com>
2026-08-31 10:15:17 -0400

    ligne mettre le processus apache en avant

--- docker/demarrer-services.sh
+++ docker/demarrer-services.sh
@@ -8,3 +8,8 @@ service mariadb start
 until mysqladmin ping --silent; do
     sleep 1
 done
+
+# 3. Premier démarrage : préparer la base (voir l'étape 02)
+
+# 4. Apache en avant-plan = PID 1 du conteneur
+exec apache2-foreground
