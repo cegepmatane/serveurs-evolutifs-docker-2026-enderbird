5c2d1f69b4fb26337d61445a5a0bbb66c939894d
ender_bird_ <cedricsimard28@gmail.com>
2026-08-31 11:10:43 -0400

    lignes pour le fichier docker pour copier les fichiers ET fix pour databases et parametres de demarrage

--- docker/demarrer-services.sh
+++ docker/demarrer-services.sh
@@ -3,6 +3,9 @@ set -e
 
 cheminFichierTemoin="/var/lib/mysql/initialise.flag"
 cheminBaseDeDonnees="/docker-entree/base-de-donnees.sql"
+DB_NAME=$(grep "DB_NAME" /var/www/html/wp-config.php | sed -E "s/.*'([^']+)'.*/\1/")
+DB_USER=$(grep "DB_USER" /var/www/html/wp-config.php | sed -E "s/.*'([^']+)'.*/\1/")
+DB_PASSWORD=$(grep "DB_PASSWORD" /var/www/html/wp-config.php | sed -E "s/.*'([^']+)'.*/\1/")
 
 # 1. Démarrer MariaDB en arrière-plan
 service mariadb start
@@ -15,12 +18,12 @@ done
 # 3. Premier démarrage : préparer la base (voir l'étape 02)
 if [ ! -f "$cheminFichierTemoin" ]; then
     # Créer la base et le compte attendus par wp-config.php
-    mysql -e "CREATE DATABASE cornucopia;
-        CREATE USER 'flora'@'localhost' IDENTIFIED BY 'potager-2026';
-        GRANT ALL PRIVILEGES ON nom-de-la-base.* TO 'flora'@'localhost';"
+    mysql -e "CREATE DATABASE $DB_NAME;
+        CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';
+        GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';"
 
     # Importer la sauvegarde de la base de Cornucopia
-    mysql cornucopia < $cheminBaseDeDonnees
+    mysql $DB_NAME < $cheminBaseDeDonnees
 
     touch "$cheminFichierTemoin"
 fi
